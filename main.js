e67a4b42ab92db92b7163b9abd059ce88ebcb0cf
Gustav von Knorring <59099761+gustavvk@users.noreply.github.com>
2022-09-28 16:57:28 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+var quiz=document.getElementById("quiz");
+var ques= document.getElementById("question");
+var tques=questions.length;
+var N_namnder = namnder.length;
+var opt = new Array(tques)
+for (let i = 0; i < N_namnder; i++){
+    var opt_string = "option" + (i+1);
+    opt[i] =document.getElementById(opt_string);
+}
+
+var res=document.getElementById("result");
+var nextbutton= document.getElementById("next");
+var q=document.getElementById('quit');
+
+
+var score= new Array(tques).fill(0);;
+var quesindex=0;
+function quit()
+{         
+	      quiz.style.display='none';
+          result.style.display='';
+          var f=score/tques;
+          result.textContent="SCORE ="+f*100;
+          q.style.display="none";
+}
+function give_ques(quesindex) 
+{
+	ques.textContent=quesindex+1+". "+questions[quesindex][0];
+	for (let i = 0; i < N_namnder; i++){
+    	    opt[i].textContent=questions[quesindex][i+1];
+	}
+
+	 return;// body...
+};
+give_ques(0);
+function nextques()
+{
+	var selected_ans= document.querySelectorAll('input:checked');
+	console.log(selected_ans);
+	if(!selected_ans)
+		{alert("Välj åtminstonde ett alternativ");return;}
+	
+	for (let i = 0; i < selected_ans.length; i++) {
+ 		namnd_index = selected_ans.item(i).value;
+		score[namnd_index]+=1/tques;
+		selected_ans.item(i).checked=false;
+	} 
+	
+	     quesindex++;
+	     if(quesindex==tques-1)
+	     	nextbutton.textContent="Finish";
+	     if(quesindex==tques)
+	     {
+	     q.style.display='none';
+          quiz.style.display='none';
+          result.style.display='';
+          var final_score="<b>Resultat</b>: <br>";
+          for (let i = 0; i < N_namnder; i++) {
+          	final_score += namnder[i] + ": " + (score[i]*100) + "%<br>";
+          }
+          result.innerHTML= final_score;
+          console.log(final_score)
+            return;
+	     }
+        give_ques(quesindex);
+
+}
---
+++ questions.js
@@ -0,0 +1,16 @@
+var questions = [
+  ["Vad gör dig tillfredsställd?", "Jämlikhet", "Att vara ekonomiskt oberoende. Ännu bättre om mina vänner också är det!", "När folk behöver din hjälp för att göra det de så gärna vill","En enkel lösning till komplexa tekniska problem", "Lära känna nya främmande människor", "Rena bänkar, rent golv, när andra människor har roligt, glada gäster, prata med folk.", "Att se människor jobba tillsammans för att skapa något stort!", "När allt bara funkar", "Ordning och reda"],
+  ["Vad gör du helst en fredagskväll?", "Kollar svt-dokumentär om samhällsproblem", "Jobbar sent, därefter drar jag ut och slirar på Stureplan tills ännu senare", " Har det roligt med kompisar, oavsett vad vi gör", "Programmera","Kollar på en film/serie från ett främmande land", "Kollar LEG och väskor i dörren. Luktar på skumma vätskor....", "Ligger på soffan och tittar på film, kanske blir det en punsch i pausen", "Käka pizzor, kollar på film och slipper skicka in en inlämning", "Myser"],
+  ["Vilka vill du bossa över?", "the Tesseract", "the Magic Cube", "the Mjolnir", "all","1"],
+  ["Jobbar du helst på egen hand eller i grupp?", " 4", " 10", "6", "100","3"],
+  ["Hur mycket tid vill du lägga på sektionssaker?", "  they were bitten by a radioactive spider", " they were exposed to cosmic radiation in space", " they were experimented on", "Dont know","2"],
+  ["Vilket av följande citat stämmer bäst på dig?", "  Stark and Sons", "  Stark and Co", " Stark Industries", "Avg co","3"],
+  ["Vilken av följande situationer ser du dig helst i?", " Elektra", "  Storm", "The Invisible Woman", "Wonder Women","1"],
+  ["Vad skulle du vilja bidra till på Fysiksektionen?", " Nearly 100 years", " Nearly 70 years", "Nearly 20 years", "Nearly 10 years","2"],
+  ["Vilka tidsintervall är du vaken på?", "Jane Foster", "Jean Grey", "Mary Jane", "peta Jenson","1"],
+  ["Om Fysiksektionen fick en miljon kronor till, vad skulle du vilja göra med dem?", "The Nightcrawler", "The Silver Surfer", "The beast", "Monkey Man","3"],
+  ["En ska vara...", " The Black Widow", "The Invisible Woman", "The Phoenix", "The Hulk","2"],
+  ["Jag skulle inte klara mig utan...", "Death Proof", "  War Machine", " Killing Machine", "Jump Suit","1"],
+  ["Det här vill jag få ut av mitt engagemang", "Death Proof", "  War Machine", " Killing Machine", "Jump Suit","1"],
+  ];
+var namnder = ["Jämlikhetsnämnden - Ojämn", "Fysiksektionens Näringslivsnämnd", "Aktivitetsnämnden, FAN", "F.dev", "FINT", "fkm*", "Fysikalen", "FSN", "FRum"]
