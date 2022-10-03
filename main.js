576726ffb99754694610c2331c0a635291680980
Samuel <sael@kth.se>
2022-10-03 19:53:17 +0200

    Fixade 1-5 frågorna

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var quiz=document.getElementById("quiz");
 var ques= document.getElementById("question");
 var tques=questions.length;
 var N_namnder = namnder.length;
+var N_radio_ques = 5;
+var do_radio;
 
 // välj antalet nollskilda toppmatchningar som visas i slutet här (vid samma ranking visas alla även om det blir fler)
 var N_resultat = 5;
@@ -9,11 +11,16 @@ var N_resultat = 5;
 // Text som står efter resultatet
 var sluttext = '<br>Sugen på att veta mer om någon nämnd*? Kolla <a href="https://docs.google.com/document/d/1gP65qTX5yvGZpYiMBMD_K9XyRCzFmDqCV0N7Uzu-Vdg/edit?usp=sharing", target="_blank">här</a>'
 
-var opt = new Array(tques)
+var opt_checkbox = new Array(tques);
 var question_order = [...Array(N_namnder).keys()]
 for (let i = 0; i < N_namnder; i++){
     var opt_string = "option" + (i+1);
-    opt[i] =document.getElementById(opt_string);
+    opt_checkbox[i] =document.getElementById(opt_string);
+}
+var opt_radio = new Array(N_radio_ques);
+for (let i = 0; i < N_radio_ques; i++){
+    var opt_string = "radio" + (i+1);
+    opt_radio[i] = document.getElementById(opt_string);
 }
 
 var res=document.getElementById("result");
@@ -27,8 +34,27 @@ var quesindex=0;
 function give_ques(quesindex) 
 {
 	ques.textContent=quesindex+1+". "+questions[quesindex][0];
+	do_radio = Number.isInteger(questions[quesindex][1]);
+	
 	for (let i = 0; i < N_namnder; i++){
-    	    opt[i].textContent=questions[quesindex][question_order[i]+1];
+	    if(do_radio){
+	        opt_checkbox[i].parentNode.style.display = 'none';
+	    }
+	    else{
+	        opt_checkbox[i].parentNode.style.display = 'inline-block';
+	    }
+	    opt_checkbox[i].textContent=questions[quesindex][question_order[i]+1];
+
+	}
+	for (let i=0; i < N_radio_ques; i++){
+	    if(!do_radio){
+	        opt_radio[i].parentNode.style.display = 'none';
+	    }
+	    else{
+	        opt_radio[i].parentNode.style.display = 'inline-block';
+	    }
+	    opt_radio[i].textContent="" + (i+1);
+	 
 	}
 
 	 return;// body...
@@ -40,11 +66,25 @@ function nextques()
 	if(selected_ans.length==0)
 		{alert("Välj åtminstonde ett alternativ");return;}
 	
-	for (let i = 0; i < selected_ans.length; i++) {
+	//beräkna score
+	if(do_radio){
+	    for (let i = 0; i < N_namnder; i++) {
+ 		if (questions[quesindex][i]==selected_ans[0].value){
+ 		    score[i]+=1/tques;
+ 		}
+ 		
+	    }
+	    selected_ans.item(0).checked=false;
+	    
+	}
+	else{
+	    for (let i = 0; i < selected_ans.length; i++) {
  		namnd_index = question_order[selected_ans.item(i).value];
 		score[namnd_index]+=1/tques;
 		selected_ans.item(i).checked=false;
-	} 
+	    } 
+	
+	}
 	
 	     quesindex++;
 	     if(quesindex==tques-1)
